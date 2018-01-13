b5a12ee06c7f891c058073e2a493bda982f51d42
David Kes <davidkes@Davids-MacBook-Pro.local>
2018-01-12 16:04:36 -0800

    faster

--- flipimage.py
+++ flipimage.py
@@ -5,28 +5,23 @@ from PIL import ImageOps
 import glob
 
 # path of file
-path='/Users/davidkes/classes/697/Project/wiki_crop/'
+path='/Users/davidkes/classes/697/Project/wikitemp/'
 # where to put the dump the mirror images
-floc = '/Users/davidkes/classes/697/Project/dump/'
+floc = '/Users/davidkes/classes/697/Project/dump1/'
 def read_files_from_path(path,floc):  # function to read files and add date components
-    my_dir = [name for name in os.listdir(path) if os.path.isdir(os.path.join(path, name))]
     try:
         os.mkdir(floc)
     except:
         pass
-
-    for i in my_dir:
-        my_sub_dir = glob.glob(path + i + '/*.jpg')
-        filepathname = floc + '1' + i + '/'
+    my_sub_dir = glob.glob(path +'*/*.jpg')
+    for j in my_sub_dir:
+        img = Image.open(j)
+        mirror_img = ImageOps.mirror(img)
+        inx = j.split('/')
         try:
-            os.mkdir(filepathname)
+            os.mkdir(floc+'1'+inx[-2])
         except:
             pass
-        for j in my_sub_dir:
-            img = Image.open(j)
-            mirror_img = ImageOps.mirror(img)
-            mirror_img.save(filepathname + j.split('/')[-1])
-
+        mirror_img.save(floc + '1'+'/'.join(inx[-2:]))
 
 read_files_from_path(path,floc)
-
